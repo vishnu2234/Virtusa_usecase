80b0131b164b8d5ffe2be1705e8b6b08b40f9f24
VISHNUPRIYA R <133206788+vishnu2234@users.noreply.github.com>
2026-04-25 10:33:02 +0530

    Update Farecalc.py

--- Python_usecase/Farecalc.py
+++ Python_usecase/Farecalc.py
@@ -3,9 +3,7 @@ def calculate_fare(km, vehicle_type, hour):
         'Economy': 10,
         'Premium': 18,
         'Suv': 25
-        
     }
-
     vehicle_type = vehicle_type.capitalize()
 
     if vehicle_type not in rates:
@@ -19,20 +17,15 @@ def calculate_fare(km, vehicle_type, hour):
     if 17 <= hour <= 20:
         surge = base_fare * 0.5
 
-    # Night charge
-    night_charge = 0
-    if hour >= 22:
-        night_charge = base_fare * 0.2
-
     # Discount
     discount = 0
     if km > 20:
         discount = base_fare * 0.1
 
     # Final fare
-    final_fare = base_fare + surge + night_charge - discount
+    final_fare = base_fare + surge - discount
 
-    return base_fare, surge, night_charge, discount, final_fare
+    return base_fare, surge, discount, final_fare
 
 
 try:
@@ -42,12 +35,11 @@ try:
         exit()
     vehicle_type = input("Enter vehicle type: ")
     hour = int(input("Enter hour (0-23): "))
-    if hour<0 or hour>23:
+    if hour <0 or hour>23:
         print("Hour must be between 0 and 23")
         exit()
-        
 
-    base, surge, night, discount, final = calculate_fare(km, vehicle_type, hour)
+    base, surge, discount, final = calculate_fare(km, vehicle_type, hour)
 
     if base is None:
         print(" Service Not Available")
@@ -57,7 +49,6 @@ try:
         print(f"Vehicle: {vehicle_type.capitalize()}")
         print(f"Base Fare: ₹{base}")
         print(f"Surge Charge: ₹{surge}")
-        print(f"Night Charge: ₹{night}")
         print(f"Discount: -₹{discount}")
         print(f"Final Fare: ₹{final}")
       
